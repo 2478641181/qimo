e0930c8237aa460eaa6d8bd47a0e83a342c709f6
2478641181 <2478641181@qq.com>
2025-12-23 13:12:37 +0800

    动画

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,29 @@
 import Head from 'next/head'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Analytics } from '@vercel/analytics/react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  const [isTransitioning, setIsTransitioning] = useState(false)
+
+  useEffect(() => {
+    const handleStart = () => setIsTransitioning(true)
+    const handleComplete = () => setIsTransitioning(false)
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  }, [router])
+
   return (
     <>
       <Head>
@@ -11,7 +31,9 @@ export default function App({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <Component {...pageProps} />
+      <div className={`page-transition-wrapper ${isTransitioning ? 'transitioning' : ''}`}>
+        <Component {...pageProps} />
+      </div>
       <Analytics />
       <SpeedInsights />
     </>
